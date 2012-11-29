8818e40bb199ce7c9ce9c100e6cac69b9c3ed71e
wedens <wedens13@yandex.ru>
2012-11-29 23:13:31 +0700

    added zprezto

--- install.sh
+++ install.sh
@@ -5,3 +5,11 @@ ln -s -f ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s -f ~/dotfiles/vimrc ~/.vimrc
 ln -s -f ~/dotfiles/vim ~/.vim
 ln -s -f ~/dotfiles/ackrc ~/.ackrc
+
+ln -s -f ~/dotfiles/zprezto ~/.zprezto
+ln -s -f ~/dotfiles/zlogin ~/.zlogin
+ln -s -f ~/dotfiles/zlogout ~/.zlogout
+ln -s -f ~/dotfiles/zpreztorc ~/.zpreztorc
+ln -s -f ~/dotfiles/zprofile ~/.zprofile
+ln -s -f ~/dotfiles/zshenv ~/.zshenv
+ln -s -f ~/dotfiles/zshrc ~/.zshrc
