052a0e791f18edcfa9d31d5c0c3da1b77d2dd65d
wedens <wedens13@yandex.ru>
2012-11-29 20:53:07 +0700

    installation script

---
+++ install.sh
@@ -0,0 +1,6 @@
+ln -s -f ~/dotfiles/gemrc ~/.gemrc
+ln -s -f ~/dotfiles/gitconfig ~/.gitconfig
+ln -s -f ~/dotfiles/gitignore ~/.gitignore
+ln -s -f ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s -f ~/dotfiles/vimrc ~/.vimrc
+ln -s -f ~/dotfiles/vim ~/.vim
